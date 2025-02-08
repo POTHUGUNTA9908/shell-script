80684076f9e1ff27838cae819c4edbae8b8925e9
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-08 15:47:33 +0530

    hell0 world

--- 03.variables.sh
+++ 03.variables.sh
@@ -7,5 +7,5 @@ person2=phani
 echo "person1:: Hello person2, How are you ?"
 echo "person2:: i am fine person1, how are you ?"
 echo "person1 :: i am fine too . how is your work?"
-echo "suresh:: not bad. i am thinking to upgrade my skills"
+echo "person2:: not bad. i am thinking to upgrade my skills"
 echo "person2:: good idea. i am also learning devops"
