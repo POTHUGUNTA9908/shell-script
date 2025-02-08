5eedc59b4c400100055c0c961cc4edf0e20090d8
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-08 15:32:39 +0530

    hell0 world

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+
+
+
+echo "Ramesh:: Hello suresh, How are you ?"
+echo "suresh:: i am fine ramesh, how are you ?"
+echo "ramesh :: i am fine too . how is your work?"
+echo "suresh:: not bad. i am thinking to upgrade my skills"
+echo "ramesh:: good idea. i am also learning devops"
