bb462bfa7cae141c7480f982a902d16f3f46b285
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-08 17:18:40 +0530

    hell0 world

--- 05.variables.sh
+++ 05.variables.sh
@@ -10,3 +10,17 @@ echo "please enter password ::"
 read -s  password 
 
 echo "username is : $username, password is : $password "
+
+
+#!/bin/bash
+
+
+
+echo "please enter username ::"
+
+read username 
+
+echo "please enter password ::"
+read password
+
+echo username :$username, password : $password 
