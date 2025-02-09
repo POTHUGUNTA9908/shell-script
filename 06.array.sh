6aa05a5da037aa3196d69ace79c2a9ac0994736c
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-09 09:47:05 +0530

    hell0 world

--- 06.array.sh
+++ 06.array.sh
@@ -3,6 +3,6 @@
 
 Movies=("RRR" "DJTILLU" "MURALI")
 
-echo"first movie is: ${Movies[0]}"
-echo"first movie is: ${Movies[1]}"
-echo"first movie is:  ${Movies[2]}"
+echo "first movie is: ${Movies[0]}"
+echo "first movie is: ${Movies[1]}"
+echo "first movie is:  ${Movies[@]}"
