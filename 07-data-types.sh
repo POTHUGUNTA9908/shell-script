059db58d7f94934aa07bc4ce0c07aa8f05e955b2
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-09 10:08:47 +0530

    hell0 world

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -5,6 +5,16 @@ NO1=$1
 No2=$2
 
 
-sum=$(($No1 + $No2))
+sum=$(($No1+$No2))
 
-echo "Total of $No1 and $No2 is $sum" 
+echo "Total of $No1 and $No2 is: $sum" 
+
+
+#!/bin/bash
+
+NO1=$1
+NO2=$2
+
+SUM=$(($NO1+$NO2))
+
+echo "Total of $NO1 and $NO2 is: $SUM"
