488007df596312533103729ba20f4fbb479cde57
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-09 12:04:44 +0530

    hell0 world

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -4,9 +4,8 @@
 Number =$1
 
 then
-echo "Given number $Number is greather than 10"
+echo "Given number $Number is greater  than 10"
 
 else 
-  echo "Given number $Number is less than 10 "
+  echo "Given number $Number is less than 10"
   fi
-  
