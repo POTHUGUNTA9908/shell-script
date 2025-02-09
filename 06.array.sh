2ec4acfc92f3f4898305c8289e98d3444d9acb39
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-09 09:55:10 +0530

    hell0 world

--- 06.array.sh
+++ 06.array.sh
@@ -4,6 +4,6 @@
 movies=("rrr" "sairam" "rakesh")
 
 
-echo "first movies is: ${movies[0]}
-echo "first movies is: ${movies[1]}
-echo "first movies is: ${movies[2]}
+echo "first movies is: ${movies[0]}"
+echo "first movies is: ${movies[1]}"
+echo "first movies is: ${movies[2]}"
