967fd913fd88925900d1399532f2965962dea058
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-13 08:03:31 +0530

    first open

--- 14.install packages.sh
+++ 14.install packages.sh
@@ -19,7 +19,7 @@ VALIDATE() {
         echo "$2...$G success $N"
     fi
 }
-
+      if [ $1 -ne 0 ]
 then
     echo "Please run this script with root access."
     exit 1 # manually exit if error comes.
