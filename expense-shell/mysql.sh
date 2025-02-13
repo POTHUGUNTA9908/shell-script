421ed0e48b5d283dcbd83b21f7b04ef49681bf4d
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-13 11:08:47 +0530

    first open

--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -33,10 +33,10 @@ fi
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "installing mysql server"
 
-systemctl enable mysqld -y &>>$LOGFILE
+systemctl enable mysqld  &>>$LOGFILE
 VALIDATE $? "enabling mysql server"
 
-systemctl start mysqld -y &>>$LOGFILE
+systemctl start mysqld  &>>$LOGFILE
 VALIDATE $? "starting mysql server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
