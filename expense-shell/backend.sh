86dc6579884f6ff9796f2d06c0557bfcd6304b04
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-14 09:26:56 +0530

    expense shell

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -41,7 +41,7 @@ dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "installing nodejs "
 
 id expense &>>$LOGFILE
- if ( $? -ne 0 )
+ if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE
     VALIDATE $? "creating expense user"
