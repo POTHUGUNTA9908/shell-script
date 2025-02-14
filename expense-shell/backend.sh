56137635d80bcff7d7fd2f2bf64d649fe8eb38aa
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-14 07:20:24 +0530

    expense shell

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -42,3 +42,25 @@ VALIDATE $? "installing nodejs "
 
 useradd expense
 VALIDATE $? "creating expense user"
+
+mkdir -p /app
+VALIDATE $? "creating /app directory"
+
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading backend code"
+
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Extracting backend code"
+
+npm install
+VALIDATE $? "installing node js dependicies"
+
+systemctl daemon-reload
+
+systemctl start backend
+
+
+systemctl enable backend
