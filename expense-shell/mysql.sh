3d274dc1145bb5910e0bbea59418dd868751901b
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-14 06:03:39 +0530

    expense shell

--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -39,5 +39,14 @@ VALIDATE $? "enabling mysql server"
 systemctl start mysqld  &>>$LOGFILE
 VALIDATE $? "starting mysql server"
 
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "setting up root password"
+
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "setting up root password"
+
+if [ $? -ne 0]
+then 
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    VALIDATE $? "setting up root root password"
+else
+    echo  -e "mysql root password is already setup ...$Y skipping $N"
