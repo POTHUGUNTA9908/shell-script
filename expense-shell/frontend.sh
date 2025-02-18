c93408b998b9d5d528fcd145515269eedb351659
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-18 10:09:50 +0530

    first  message

--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -47,7 +47,8 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/shell-script/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
