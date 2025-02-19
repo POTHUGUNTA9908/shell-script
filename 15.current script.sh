a1b6fc5dfa61c66e15e05c3a41596bcdad558c31
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-19 06:04:39 +0530

    first go

--- 15.current script.sh
+++ 15.current script.sh
@@ -1,13 +1,27 @@
 
+# #!/bin/bash
+
+# COURSE="DevOps from Current Script"
+
+# echo "before calling other script,course: $course"
+
+# echo "process id of current shell script : $$"
+
+# ./16.otherscript.sh
+
+
+# echo "after calling other script,course:$course"
+
+
 #!/bin/bash
 
 COURSE="DevOps from Current Script"
 
-echo "before calling other script,course : $course"
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
 
-echo "process id of current shell script : $$"
+./16-other-script.sh
 
-./16.otherscript.sh
+# source ./16-other-script.sh
 
-
-echo "after calling other script,course : $course"
+echo "After calling other script, course: $COURSE"
--- 16.otherscript.sh
+++ 16.otherscript.sh
@@ -1,8 +1,15 @@
 
+# #!/bin/bash
+
+# COURSE="DevOps from Other Script"
+
+# echo "variable value from otherscript : $course"
+
+# echo "process id of other script : $$"
+
 #!/bin/bash
 
 COURSE="DevOps from Other Script"
 
-echo "variable value from otherscript : $course"
-
-echo "process id of other script : $$"
+echo "Variable value from Other script: $COURSE"
+echo "Process ID of other script: $$"
