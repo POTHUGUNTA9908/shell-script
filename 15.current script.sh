f5a2c7338922fb9ffb166eb0c42167f0ca071a0f
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-19 05:45:46 +0530

    first go

---
+++ 15.current script.sh
@@ -0,0 +1,15 @@
+
+
+
+#!/bin/bash
+
+course ="Devops from cvurrent script"
+
+echo "before calling other script,course:$course"
+
+echo "process id of current shell script : $$"
+
+# ./16.otherscript.sh
+
+
+echo "after calling other script,course:$course"
---
+++ 16.otherscript.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+course ="Devops from otherscript"
+
+echo "variable value from otherscript:$course"
+
+echo "process id of other script : $$"
