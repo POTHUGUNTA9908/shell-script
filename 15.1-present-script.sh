156a551dde6c4c8825fb6943ba255235218541eb
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-22 19:43:17 +0530

    push

--- 15.1-present-script.sh
+++ 15.1-present-script.sh
@@ -6,6 +6,7 @@ echo "before calling  present script ; course : $course"
 
 echo "process id of present script : $$"
 
-./16.1-future-script.sh
+# ./16.1-future-script.sh
+ source ./16.1-future-script.sh
 
 echo "after calling future-script ; course : $course"
