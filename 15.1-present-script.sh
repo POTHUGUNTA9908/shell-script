f1d7870ebbf6f3e1740c53325247923ee0747ca7
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-22 19:37:12 +0530

    push

---
+++ 15.1-present-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+course= "Devops from present script"
+
+echo "before calling  present script ; course : $course"
+
+echo "process id of present script : $$"
+
+./16.1-future-script.sh
+
+echo "after calling future-script ; course : $course"
---
+++ 16.1-future-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+course="Devops from future script"
+
+echo "after calling future script : $course"
+
+echo "process id of future script : $$"
