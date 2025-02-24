3bfb13cff2f39b3822d0ad6a02757a5ce743104f
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-24 06:59:08 +0530

    first move

--- 18.delete-old-logs.sh
+++ 18.delete-old-logs.sh
@@ -5,10 +5,11 @@ source_directory=/tmp/app-logs
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 
-if [ -d "$source_directory" ]; then
-  echo -e"$G $source_directory exist $N"
+if [ -d "$source_directory" ]
+  echo -e"$G source_directory exist $N"
   else 
   echo -e"$R please make sure $source_directory exist $N"
    exit 1
