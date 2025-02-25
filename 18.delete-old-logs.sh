50e4cea064ccb6a65e22a8682792ab5f3bb0a9a7
pothuguntarakesh <pothugunta.rakesh.com>
2025-02-25 07:14:21 +0530

    first plan

--- 18.delete-old-logs.sh
+++ 18.delete-old-logs.sh
@@ -1,26 +1,52 @@
+# #!/bin/bash
+
+#  source_directory=/tmp/app-logs
+
+#  R="\e[31m"
+#  G="\e[32m"
+#  N="\e[0m"
+#  Y="\e[33m"
+
+
+#  if [ -d "$source_directory" ]
+#     then
+#    echo -e "$G source directory exists $N"
+#   else 
+#    echo -e "$R please make sure $source_directory exists $N"
+#    exit 1
+#  fi
+
+# FILES=$(find $source_directory -name "*.log" -mtime +14)
+
+
+# While IFS= read -r line
+# do
+#    echo "Deleting file: $line"
+#    rm -rf $line
+# done <<< $FILES
+
+
 #!/bin/bash
 
- source_directory=/tmp/app-logs
+SOURCE_DIRECTORY=/tmp/app-logs
 
- R="\e[31m"
- G="\e[32m"
- N="\e[0m"
- Y="\e[33m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G Source directory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
 
- if [ -d "$source_directory" ]
-    then
-   echo -e "$G source directory exists $N"
-  else 
-   echo -e "$R please make sure $source_directory exists $N"
-   exit 1
- fi
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
-FILES=$(find $source_directory -name "*.log" -mtime +14)
-
-
-While IFS= read -r line
+while IFS= read -r line
 do
-   echo "Deleting file: $line"
-   rm -rf $line
-done <<<$FILES
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES
